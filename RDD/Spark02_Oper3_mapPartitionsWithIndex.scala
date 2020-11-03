416db749a1c97e64f0b461876f3833444dba9232
RyanZZD <37041449+RyanZZD@users.noreply.github.com>
2020-11-03 16:44:57 +0800

    Update and rename Spark02_Oper3.scala to Spark02_Oper3_mapPartitionsWithIndex.scala

--- RDD/Spark02_Oper3_mapPartitionsWithIndex.scala
+++ RDD/Spark02_Oper3_mapPartitionsWithIndex.scala
@@ -12,7 +12,6 @@ object Spark02_Oper3 {
     //map算子
     val listRDD: RDD[Int] = sc.makeRDD(1 to 10, 2)
 
-
     //创建一个RDD，使每个元素跟所在分区形成一个元组组成一个新的RDD
     val tupleRDD: RDD[(Int, String)] = listRDD.mapPartitionsWithIndex {
       case (num, datas) => {
